05c333763059b7a7f95f5767669dd403aa6dc035
Tim Kao <pakuya.kao@gmail.com>
2019-04-06 13:55:06 -0400

    python recursive practice

--- pythonAns/preOrderTraversal.py
+++ pythonAns/preOrderTraversal.py
@@ -10,3 +10,16 @@ class Solution:
                 stack.append(curNode.left)
                 result.append(curNode.val)
         return result
+
+class Solution2:
+
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        def dfs(node):
+            if node == None:
+                return
+            result.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return result
